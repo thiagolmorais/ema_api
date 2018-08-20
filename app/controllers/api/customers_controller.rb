e29f7cc598708ab7ba93d671bc86c397120ca887
Thiago Fernando <thiago.fernando@locaweb.com.br>
2018-08-19 22:21:06 -0300

    Add customer show

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -4,4 +4,12 @@ class Api::CustomersController < ApplicationController
     json = customers.as_json
     render json: json
   end
+
+  def show
+    customer = Customer.find(params[:id])
+    json = customer.as_json
+    render json: json
+  rescue ActiveRecord::RecordNotFound
+    render json: 'Nenhum cliente encontrado', status: 404
+  end
 end
---
+++ spec/requests/get_customers_id_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+describe 'Customers API', type: :request do
+  it 'see a specific customers registered' do
+    customer_a = create(:customer, name: 'Custumer A',
+                                   email: 'customer.a@email.com',
+                                   phone: '123456789', status: true)
+    customer_b = create(:customer, name: 'Custumer B',
+                                   email: 'customer.b@email.com',
+                                   phone: '987654321', status: false)
+
+    get api_customer_path(customer_b)
+    data = JSON.parse(response.body)
+    expect(response.status).to eq 200
+    expect(data['name']).to eq customer_b.name
+    expect(data['email']).to eq customer_b.email
+    expect(data['phone']).to eq customer_b.phone
+    expect(data['status']).to eq customer_b.status
+  end
+
+  it 'and see a message if no customer_id found' do
+      get api_customer_path(1)
+
+      expect(response.body).to eq 'Nenhum cliente encontrado'
+      expect(response.status).to eq 404
+    end
+
+end
