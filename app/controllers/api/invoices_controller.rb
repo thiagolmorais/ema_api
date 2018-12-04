dd441c269ed847b06b417d013488add26e9fbc78
Thiago Fernando <thiago.fernando@locaweb.com.br>
2018-12-03 23:38:10 -0200

    Add customer in the invoice

--- app/controllers/api/invoices_controller.rb
+++ app/controllers/api/invoices_controller.rb
@@ -1,6 +1,10 @@
 class Api::InvoicesController < ApplicationController
   def index
     invoices = Invoice.all.order(:due_date)
+    invoices = invoices.map do |invoice|
+      # binding.pry
+       invoice.as_json.merge(customer: invoice.customer)
+    end
     json = invoices.as_json
     render json: json
   end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
 class Invoice < ApplicationRecord
   belongs_to :appointment
+  has_one :customer, through: :appointment
 end
