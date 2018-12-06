0ee2fce6b481ac820c26c5243a0a3ad9574d1b3b
Thiago Fernando <thiago.fernando@locaweb.com.br>
2018-12-06 17:19:09 -0200

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,7 @@ customer_a = Customer.create(name: 'Customer A', email: 'customer.a@email.com',
                              phone: '123456789', status: true)
 customer_b = Customer.create(name: 'Customer B', email: 'customer.b@email.com',
                              phone: '987654321', status: false)
-Price.create(price: '150.99',readjust: '2018-08-22', customer_id: customer_a.id)
-Price.create(price: '150.99',readjust: '2018-08-22', customer_id: customer_b.id)
-first_appointment = Appointment.create(start_time: Time.now, end_time: Time.now+3600, status: true, price: 150.00, customer_id: customer_a.id)
-second_appointment = Appointment.create(start_time: Time.now-1.day, end_time: Time.now-1.day+3600, status: true, price: 150.00, customer_id: customer_a.id)
-Invoice.create(appointment_id: first_appointment.id, value: first_appointment.price, competence: "Abril/2018", due_date: '2018-05-05', payment: '2018-05-05', status: true)
-Invoice.create(appointment_id: second_appointment.id, value: second_appointment.price, competence: "Abril/2018", due_date: '2018-05-09', payment: nil, status: false)
+price_a = Price.create(price: '150.99',readjust: '2018-01-22', customer_id: customer_a.id)
+price_b = Price.create(price: '180.99',readjust: '2018-01-01', customer_id: customer_b.id)
+first_appointment = Appointment.create(start_time: "2018-11-06T18:37:31.878Z", end_time: "2018-11-06T19:37:31.878Z", status: false, price: price_a.price, customer_id: customer_a.id)
+second_appointment = Appointment.create(start_time: "2018-11-16T18:37:31.878Z", end_time: "2018-11-16T18:37:31.878Z", status: false, price: price_b.price, customer_id: customer_a.id)
